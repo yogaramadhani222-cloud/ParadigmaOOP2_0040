80c26ab38857f6f667d23646de2375ddf2a5f092
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-05-18 10:49:08 +0700

    membuat tampilan nilai

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -12,4 +12,9 @@ class Abstraksiklas {
         x = a;
         y = b;
     }
+    //menampilkan nilai
+    void display(){
+        cout << " x: " << x << endl;
+        cout << " y: " << y << endl;
+    }
 };
