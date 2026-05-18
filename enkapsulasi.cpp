22335720d7f2d00c6dcbe2f0be42a43b49e5932b
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-05-18 10:41:42 +0700

    membuat dan isi fungsi main dengan memasukan data dan tampilan data

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -12,3 +12,17 @@ public:
         return saklarNo[i];
     }
 };
+
+int main()
+{
+    remoteLampu lampuRumah;
+    lampuRumah.setSaklarNo(0, "Saklar 1");
+    lampuRumah.setSaklarNo(1, "Saklar 2");
+    lampuRumah.setSaklarNo(2, "Saklar 3");
+
+    cout << lampuRumah.getSaklarNo(0) << endl;
+    cout << lampuRumah.getSaklarNo(1) << endl;
+    cout << lampuRumah.getSaklarNo(2) << endl;
+
+    return 0;
+}
