7297e78699f11de0b39cae580f8b3fe17c776d85
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-05-18 10:48:28 +0700

    buat private number dan method untuk mengisi nilai x dan y

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -2,4 +2,14 @@
 using namespace std;
 
 class Abstraksiklas {
+    private: string x, y;
+
+    public:
+
+    //method untuk mengisi nilai
+    //private number
+    void setXY(string a, string b){
+        x = a;
+        y = b;
+    }
 };
